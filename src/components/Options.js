8e173c429e7100424b30b8ce2ec9dfd8d4cc7d33
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-05-27 11:39:17 -0700

    Implemented a logic when there are no options

--- src/components/Options.js
+++ src/components/Options.js
@@ -6,6 +6,9 @@ const Options = props => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Delete All</button>
+      {props.options.length === 0 && (
+        <p>Please add an option to get started!</p>
+      )}
       {props.options.map(option => (
         <Option
           key={option}
